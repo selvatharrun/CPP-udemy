6861c98339aa2aeeb79c1552e20c94d5120274a7
S.SELVA THARRUN <82883200+selvatharrun@users.noreply.github.com>
2025-04-07 05:42:12 +0000

    Updated C++ files with vectors, arrays and carpet cleaning programs

--- test1.cpp
+++ test1.cpp
@@ -19,33 +19,5 @@ int main(){
     */
 
     /*write a pro*/
-
-        vector<int> vector1;
-        vector<int> vector2;
-
-        vector1.push_back(10);
-        vector1.push_back(20);
-        cout<<"vector 1 first ele and size:"<<endl;
-        cout << vector1.at(0) << endl;
-        cout << vector1.size() << endl;
-
-        vector2.push_back(100);
-        vector2.push_back(200);
-        cout<<"vector 2 first ele and size:"<<endl;
-        cout << vector2.at(0) << endl;
-        cout << vector2.size() << endl;
-
-        vector<vector<int>> vector2d;
-        vector2d.push_back(vector1);
-        vector2d.push_back(vector2);
-        
-        // Print 2D vector
-        for(const auto& row : vector2d) {
-            for(const auto& elem : row) {
-                cout << elem << " ";
-            }
-            cout << endl;
-        }
-        return 0;
     
 }
--- vectors.cpp
+++ vectors.cpp
@@ -1,11 +1,12 @@
 /*write a pro*/
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(){
-    vector vector1<int> {};
-    vector vector2<int> {};
+    vector<int> vector1;
+    vector<int> vector2;
 
     vector1.push_back(10);
     vector1.push_back(20);
@@ -19,7 +20,16 @@ int main(){
     cout << vector2.at(0) << endl;
     cout << vector2.size() << endl;
 
-    vector<int> vector2d {}{};
+    vector<vector<int>> vector2d;
     vector2d.push_back(vector1);
     vector2d.push_back(vector2);
+
+    // Print 2D vector
+    for(const auto& row : vector2d) {
+        for(const auto& elem : row) {
+            cout << elem << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
